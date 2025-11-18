3764a686535ddcb06d2483a57d9829153003ce52
kulmonivoth <kulmonivoth@mcnc.co.kr>
2025-11-18 16:25:21 +0700

    Initial commit: React app with Open Graph meta tags for thumbnail sharing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Test Messenger</h1>
+        <p>Secure Banking Communication Platform</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
